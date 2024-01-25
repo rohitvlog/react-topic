d4912dec1187bdc48c1ef52b61cc0e1952dba457
Rohit Sharma <82994526+rohitvlog@users.noreply.github.com>
2024-01-25 12:47:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Abc from './components/abc';
+import Xyz from './components/xyz';
+import Form from './components/pages/form';
+
+function App() {
+  return (
+    <>
+    {/* <Abc/> */}
+  {/* <Xyz name = {'Rohit'}/> */}
+   <Form/>
+    </>
+  
+  );
+}
+
+export default App;
